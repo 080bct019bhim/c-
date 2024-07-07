c1db4beee1e68b250218fc6f43ffb030523e2eb9
080bct019bhim <157277291+080bct019bhim@users.noreply.github.com>
2024-07-07 08:15:31 +0545

    Update 5.cpp

--- assignment 2/5.cpp
+++ assignment 2/5.cpp
@@ -1,19 +1,41 @@
 #include<iostream>
 using namespace std;
 class vehicle{
-    public:
     char modelNumber[20];
     char modelName[20];
     double cost;
+    public:
+    void setmodelNumber()
+    {
+        cin>>modelNumber;
+    }
+    void setmodelName()
+    {
+        cin>>modelName;
+    }
+    void setcost()
+    {
+        cin>>cost;
+    }
+    void showData()
+    {
+        cout<<"Model number : "<<modelNumber<<" , Model name : "<<modelName<<" , cost : "<<cost<<endl;
+    }
 };
 int main()
 {
     vehicle v1,v2;
     cout<<"Enter Model number, model name, cost of first vehicle : ";
-    cin>>v1.modelNumber>>v1.modelName>>v1.cost;
-    cout<<"\nEnter Model number, model name, cost of second vehicle : ";
-    cin>>v2.modelNumber>>v2.modelName>>v2.cost;
-    cout<<"For first vehicle:\n"<<v1.modelNumber<<endl<<v1.modelName<<endl<<v1.cost<<endl;
-    cout<<"\nFor second vehicle:\n"<<v2.modelNumber<<endl<<v2.modelName<<endl<<v2.cost;
+    v1.setmodelNumber();
+    v1.setmodelName();
+    v1.setcost();
+    cout<<"Enter Model number, model name, cost of second vehicle : ";
+    v2.setmodelNumber();
+    v2.setmodelName();
+    v2.setcost();
+    cout<<"\n\n\n For first vehicle : \n";
+    v1.showData();
+    cout<<"\n\n\n For second vehicle : \n";
+    v2.showData();
     return 0;
 }
